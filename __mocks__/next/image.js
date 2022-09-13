dcbe38525a07db25a194b60a56778025abe08066
himchan94 <himchan9494@gmail.com>
2022-09-13 20:46:48 +0900

    feat: Add image provider in mocks

--- __mocks__/next/image.js
+++ __mocks__/next/image.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function (props) {
+export default function image(props) {
+  // when layout is responsive
+  if (props.layout === "responsive") {
+    return (
+      <div>
+        <img {...props} style={{ width: "100%" }} />
+      </div>
+    );
+  }
+
   return <img {...props} />;
 }

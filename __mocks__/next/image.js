0f6c0225ad34b0f6b7571542c541f21c2a5577a3
himchan94 <himchan9494@gmail.com>
2022-09-19 01:31:48 +0900

    feat: Add more style to sb image provider

--- __mocks__/next/image.js
+++ __mocks__/next/image.js
@@ -1,11 +1,62 @@
 import React from "react";
 
 export default function image(props) {
+  console.log(props);
+
+  let imgStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    boxSizing: "border-box",
+    padding: 0,
+    border: "none",
+    margin: "auto",
+    display: "block",
+    width: 0,
+    height: 0,
+    minWidth: "100%",
+    maxWidth: "100%",
+    minHeight: "100%",
+    maxHeight: "100%",
+    objectFit: props.objectFit ? props.objectFit : undefined,
+    objectPosition: props.objectPosition ? props.objectPosition : undefined,
+  };
+
+  let wrapperStyle = {
+    display: "block",
+    overflow: "hidden",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    boxSizing: "border-box",
+    margin: 0,
+    backgroudColor: "red",
+  };
+
   // when layout is responsive
   if (props.layout === "responsive") {
     return (
       <div>
         <img {...props} style={{ width: "100%" }} />
+      </div>
+    );
+  }
+
+  if (props.layout === "fill") {
+    return (
+      <div
+        style={{
+          backgroundColor: "red",
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+        }}>
+        <img {...props} style={imgStyle} />;
       </div>
     );
   }

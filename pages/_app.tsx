8de8b34aed13d739ed1fad8a32120860e1042bdb
himchan94 <himchan9494@gmail.com>
2022-09-19 23:21:50 +0900

    feat: Create _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useContext } from "react";
+import type { AppProps } from "next/app";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Layout from "../layout";
+import ModeProvider from "../hoc/ModeProvider";
+import "../styles/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <ModeProvider>
+        <Header />
+        <div className='content'>
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </ModeProvider>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;

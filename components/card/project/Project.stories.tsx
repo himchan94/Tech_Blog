fa840b72728b15629341edc3912601abf5baeac1
himchan94 <himchan9494@gmail.com>
2022-09-19 18:09:37 +0900

    feat: Change project card sb default props

--- components/card/project/Project.stories.tsx
+++ components/card/project/Project.stories.tsx
@@ -21,7 +21,7 @@ export const Default = Template.bind({});
 Default.args = {
   title: "테스트 프로젝트",
   cover:
-    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d58942dc-5191-4883-b15e-edab9e708715/coco1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220916%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220916T101901Z&X-Amz-Expires=3600&X-Amz-Signature=0e02a438048c447220f9d01198e3be1fd0113b2ccb58267b6eebaa253c585ac0&X-Amz-SignedHeaders=host&x-id=GetObject",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/440px-Golde33443.jpg",
   tags: [
     {
       id: "c6afb9f1-4440-4e5f-a8ba-df4a1617770a",

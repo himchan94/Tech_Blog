ca151b7f71f33e45b21ca8e83066daa5e792c9bc
himchan94 <himchan9494@gmail.com>
2022-09-25 23:02:27 +0900

    feat: Apply GTM initializer in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import TagManager, { TagManagerArgs } from "react-gtm-module";
 import type { AppProps } from "next/app";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Layout from "../layout";
 import ModeProvider from "../hoc/ModeProvider";
 import "../styles/index.scss";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "";
+
+  const tagManagerArgs: TagManagerArgs = {
+    gtmId,
+  };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <ModeProvider>
       <Layout>

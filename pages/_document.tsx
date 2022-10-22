2261af36e5847bdf1fb1afd96faa783a34f1a371
himchan94 <himchan9494@gmail.com>
2022-10-22 14:22:41 +0900

    feat: Apply new font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,16 @@ export default function Document() {
           href='http://fonts.cdnfonts.com/css/monument-extended'
           rel='stylesheet'
         /> */}
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&display=swap'
+          rel='stylesheet'
+        ></link>
       </Head>
       <body>
         <Main />
